f22a6badfac7ff187130e010298439f14446667e
AlecGabriel <93282097+AlecGabriel@users.noreply.github.com>
2023-10-29 10:06:34 +0000

    updated jobs portion

--- app.py
+++ app.py
@@ -2,9 +2,42 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': '45k'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': '35k'
+  },
+  {
+    'id': 3,
+    'title': 'Front End Engineer',
+    'location': 'Remote',
+    'salary': '120k'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Fransisco, USA',
+    'salary': '110k'
+  } 
+]
+
+
+
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
-
+  return render_template('home.html',
+                          jobs=JOBS,
+                          company_name='Alec'
+                        )
+  
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
