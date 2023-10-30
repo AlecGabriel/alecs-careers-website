e28d5ce8d8eb32d8ee9dafff24e66251f2962af3
AlecGabriel <93282097+AlecGabriel@users.noreply.github.com>
2023-10-30 10:02:39 +0000

    Added dynamic data.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -38,6 +38,11 @@ def hello_world():
                           jobs=JOBS,
                           company_name='Alec'
                         )
-  
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
+
+
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
